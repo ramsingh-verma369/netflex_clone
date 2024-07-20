665e475761d0dc976a66a9e4b75f26184e8208a7
ramsingh-verma369 <ramsinghverma9450@gmail.com>
2024-07-20 19:15:12 +0530

    Normalize all the line endings

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+export const signup = async (req,res) => {
+    res.send("Sign up function")
+};
+
+export const login = async (req,res)=> {
+    res.send("Login route");
+}
+
+export const logout = async (req,res) => {
+    res.send("Logout route")
+}
+
+
+
+// export async function logout (req,res) {
+//     res.send("Logout route")
+// }
