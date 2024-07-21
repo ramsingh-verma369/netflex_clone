484dd71c3121067bf6327c7a271ae372c0309503
ramsingh-verma369 <ramsinghverma9450@gmail.com>
2024-07-21 10:19:20 +0530

    login logic in auth controller and create jwt token

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,20 +1,88 @@
-export const signup = async (req,res) => {
-    try {
-        
-    } catch (error) {
-        
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import { generateTokenAndCookie } from "../utils/generateToken.js";
+
+export const signup = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
     }
+
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid email",
+      });
+    }
+
+    if (password.length < 6) {
+      return res.status(400).json({
+        success: false,
+        message: "Password must be atleast 6 characters",
+      });
+    }
+
+    const existingUserByEmail = await User.findOne({ email: email });
+    if (existingUserByEmail) {
+      return res.status(400).json({
+        success: false,
+        message: "Email already exists",
+      });
+    }
+
+    const existingUserByUsername = await User.findOne({ username: username });
+    if (existingUserByUsername) {
+      return res.status(400).json({
+        success: false,
+        message: "Username is already exists",
+      });
+    }
+
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+
+    const PROFILE_PICS = ["/avatar1.png", "/avatar2.png", "avatar3.png"];
+
+    const image = PROFILE_PICS[Math.random() * PROFILE_PICS.length];
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+      image,
+    });
+
+    generateTokenAndCookie(newUser._id, res);
+    await newUser.save();
+    
+    res.status(201).json({
+      success: true,
+      user: {
+        ...newUser._doc,
+        password: "",
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
 };
 
-export const login = async (req,res)=> {
-    res.send("Login route");
-}
+export const login = async (req, res) => {
+  res.send("Login route");
+};
 
-export const logout = async (req,res) => {
-    res.send("Logout route")
-}
-
-
+export const logout = async (req, res) => {
+  res.send("Logout route");
+};
 
 // export async function logout (req,res) {
 //     res.send("Logout route")
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
-import { ENV_VARS } from './config/envVars.js';
 
+
+import { ENV_VARS } from './config/envVars.js';
 import authRoutes from './routes/auth.route.js';
 import { connectDB } from './config/db.js';
 
@@ -9,6 +10,7 @@ const app = express();
 
 const PORT = ENV_VARS.PORT;
 
+app.use(express.json());
 
 app.use('/api/v1/auth',authRoutes);
 
