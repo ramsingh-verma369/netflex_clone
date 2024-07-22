fc9b53824d0a27df96469a60b483e16bae4efba7
ramsingh-verma369 <ramsinghverma9450@gmail.com>
2024-07-22 08:58:19 +0530

    complete movie routes and logic

--- backend/config/envVars.js
+++ backend/config/envVars.js
@@ -5,5 +5,6 @@ export const ENV_VARS = {
     MONGO_URI: process.env.MONGO_URI,
     PORT: process.env.PORT || 5000,
     JWT_SECRET:process.env.JWT_SECRET,
-    NODE_ENV: process.env.NODE_ENV
+    NODE_ENV: process.env.NODE_ENV,
+    TMDB_API_Key: process.env.TMDB_API_Key
 }
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from 'express';
 
+import authRoutes from './routes/auth.route.js';
+import movieRoutes from './routes/movie.route.js'
 
 import { ENV_VARS } from './config/envVars.js';
-import authRoutes from './routes/auth.route.js';
 import { connectDB } from './config/db.js';
 
 
@@ -13,6 +14,8 @@ const PORT = ENV_VARS.PORT;
 app.use(express.json());
 
 app.use('/api/v1/auth',authRoutes);
+app.use('/api/v1/movie',movieRoutes);
+
 
 
 
@@ -20,3 +23,5 @@ app.listen(PORT,() => {
     console.log("Server is started at PORT:" + PORT);
     connectDB();
 });
+
+
